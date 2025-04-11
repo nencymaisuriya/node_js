87fdee634fd2ef8c12592e6db7ea1bd0f29c37ec
NencyMaisuriya219 <160009308+nencymaisuriya@users.noreply.github.com>
2025-04-11 20:57:21 +0530

    Update index.js

--- estate/index.js
+++ estate/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const db = require('./config/db'); 
 const path = require("path");
-const multer = require('multer');
 const app = express();
-const propertyRoutes = require('./routes/propertyRoutes');
 const port = 9000;
 
 app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use('/uploads', express.static('uploads'));
-app.use('/', propertyRoutes); 
+app.use('/', require("./routes/propertyRoutes"));
 
 app.get('/', (req, res) => {
     res.render('index');
